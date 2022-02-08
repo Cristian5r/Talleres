7ccd3be01dd02fb992eff2f39c4b282699bd23b9
Cristian5r <cristian.restrepo2003@gmail.com>
2022-02-08 14:43:12 -0500

    3

--- 01variables.php
+++ 01variables.php
@@ -1,3 +1,37 @@
 <?php
-$fecha=date('d/m/y');
-echo "la fecha de hoy es $fecha";
+$fecha=date('Y');
+echo "el año presente es $fecha";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+?>
+
+<?php
+$fechaactual=2022;
+$fechanacimiento = 2003;
+$edad = $fechaactual - $fechanacimiento;
+
+echo "segun su año de nacimiento"."".$fechanacimiento.""."su edad es"."".$edad.""."años";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+?>
+
+<?php
+define("APELLIDO", "RESTREPO");
+define("NOMBRE", "CRISTIAN");
+echo constant('APELLIDO');
+echo "<br>";
+echo NOMBRE;
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+?>
+
+<?php
+$dolar=
